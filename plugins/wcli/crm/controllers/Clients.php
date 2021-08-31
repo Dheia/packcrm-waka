3eb2c722819d67f68d44cb82459ff10b834d5020
Charles <charles.stolive@gmail.com>
2021-08-31 16:00:53 +0200

    ajout word, cloud et autre modifs

--- plugins/wcli/crm/controllers/Clients.php
+++ plugins/wcli/crm/controllers/Clients.php
@@ -17,6 +17,8 @@ class Clients extends Controller
         'Waka.Mailer.Behaviors.MailBehavior',
         'Waka.ImportExport.Behaviors.ExcelImport',
         'Waka.ImportExport.Behaviors.ExcelExport',
+        'Waka.MsGraph.Behaviors.OutlookBehavior',
+        'Waka.Cloud.Behaviors.CloudWord',
     ];
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
